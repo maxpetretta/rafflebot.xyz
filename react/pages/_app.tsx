1b8792ae96507ef1c5857c748db0c2af23f3e15f
Max Petretta <git@maxpetretta.com>
2022-10-22 17:37:36 -0400

    Added ConnectKit, deleted old styles

--- react/pages/_app.tsx
+++ react/pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
+import { ConnectKitProvider, getDefaultClient } from "connectkit"
 import type { AppProps } from 'next/app'
+import { chain, createClient, WagmiConfig } from "wagmi"
+import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const client = createClient(
+  getDefaultClient({
+    appName: "rafflebot.xyz",
+    infuraId: process.env.INFURA_ID,
+    chains: [chain.hardhat, chain.goerli]
+  })
+)
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <WagmiConfig client={client}>
+        <ConnectKitProvider theme="retro">
+          <Component {...pageProps} />
+        </ConnectKitProvider>
+      </WagmiConfig>
+    </>
+  )
 }
 
-export default MyApp
+export default App
--- react/tailwind.config.js
+++ react/tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "-apple-system", "BlinkMacSystemFont", "system-ui", "sans-serif"],
+        serif: ["-apple-system-ui-serif", "ui-serif", "serif"],
+        mono: ["ui-monospace", "mono"],
+      },
+    },
   },
   plugins: [],
 }
