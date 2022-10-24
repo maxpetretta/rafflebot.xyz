1b77e4872404c68c62f2817bbeef0329f5217105
Max Petretta <git@maxpetretta.com>
2022-10-24 00:08:14 -0400

    Deployed contract to Goerli

--- react/lib/contract.ts
+++ react/lib/contract.ts
@@ -1,6 +1,6 @@
 import Rafflebot from "./abi/Rafflebot.json"
 
 export const rafflebotContract = {
-  addressOrName: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+  addressOrName: "0x411A84893aEd4fD5495e210A9f11FA009757fb98",
   contractInterface: Rafflebot.abi,
 }
